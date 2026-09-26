452b25fdfdbcc7b7d17a59bdeb0ee63fd6e56729
bojimoh <bolajijimoh8@gmail.com>
2026-09-26 09:52:18 +0100

    loops

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -3,6 +3,15 @@
 for (let i = 0; i < 5; i++) {
     console.log(i);
 }
+console.log('Loop is done!');
+console.log('--------------------');
+console.log('Loop is Finished!');
+
+const names = ['shaun', 'mario', 'luigi'];
+for (let i = 0; i < names.length; i++) {
+    console.log(i);
+    console.log(names[i]);
+} 
 
 //while loops
 
@@ -12,3 +21,16 @@ while (j < 5) {
     console.log(j);
     j++;
 }
+console.log('While loop is done!');
+console.log('--------------------');
+console.log('While loop is Finished!');
+
+const names2 = ['shaun', 'mario', 'luigi'];
+let k = 0;
+
+while (k < names2.length) {
+    console.log(k);
+    console.log(names2[k]);
+    k++;
+}
+
